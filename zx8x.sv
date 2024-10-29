6818ff35144a97dda3d57866a5757fedd77bfe9b
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2024-10-29 23:53:42 +0100

    Add QS character board, banking for CHR$128

--- zx8x.sv
+++ zx8x.sv
@@ -180,7 +180,7 @@ localparam CONF_STR =
 	"F,O  P  ,Load tape;",
 	"O4,Model,ZX80,ZX81;",
 	"OAB,RAM size,1k,16k,32k,64k;",
-	"OC,CHR128 support,Off,On;",
+	"OCD,UDG Support,Off,CHR$128,Quicksilva;",
 	"O8,Swap joy axle,Off,On;",
 	"O6,Video frequency,50Hz,60Hz;",
 	"O7,Inverse video,Off,On;",
@@ -189,6 +189,8 @@ localparam CONF_STR =
 	"V,v1.0.",`BUILD_DATE
 };
 
+wire  [1:0] st_ram = status[11:10];
+wire  [1:0] st_udg = status[13:12];
 
 ////////////////////   CLOCKS   ///////////////////
 wire clk_sys;
@@ -387,7 +389,7 @@ always @(posedge clk_sys) begin
 
 	if (reset) begin
 		zx81 <= status[4];
-		mem_size <= status[11:10];
+		mem_size <= st_ram;
 		tape_ready <= 0;
 	end
 end
@@ -415,16 +417,27 @@ wire [12:0] rom_a  = nRFSH ? addr[12:0] : { addr[12:9], ram_data_latch[5:0], row
 wire [15:0] tape_load_addr = 16'h4000 + ((ioctl_index[7:6] == 1) ? tape_addr + 4'd8 : tape_addr-1'd1);
 wire [15:0] ram_a;
 wire        ram_e_64k = &mem_size & (addr[13] | (addr[15] & nM1));
-wire        rom_e  = ~addr[14] & (~addr[12] | zx81) & ~ram_e_64k & ~chr128_mem;
-wire        ram_e  = addr[14] | ram_e_64k | chr128_mem;
+wire        rom_e  = ~addr[14] & (~addr[12] | zx81) & ~ram_e_64k & ~chr128_mem & ~chrqs_mem;
+wire        ram_e  = addr[14] | ram_e_64k | chr128_mem | chrqs_mem;
 wire        ram_we = ~nWR & ~nMREQ & ram_e;
 wire  [7:0] ram_in = tapeloader ? tape_in_byte : cpu_dout;
 wire  [7:0] rom_out;
 wire  [7:0] ram_out;
 wire  [7:0] mem_out;
 
-wire        chr128 = status[12];
-wire        chr128_mem = chr128 & (addr[15:12] == 4'h3);
+wire        chr128 = st_udg == 1;
+wire        chr128_mem = chr128 & (addr[15:13] == 3'b001);
+
+reg         chrqs_ready; // autodetect if the characters are uploaded
+always @(posedge clk_sys) begin
+	if (reset)
+		chrqs_ready <= 0;
+	else if (chrqs_mem & ~nWR)
+		chrqs_ready <= 1;
+end
+
+wire        chrqs = st_udg == 2;
+wire        chrqs_mem = chrqs & ((addr[15:10] == {4'h8, 2'b01}) || (chrqs_ready & !nRFSH)); // $8400-$87FF
 
 always_comb begin
 	casex({ tapeloader, rom_e, ram_e })
@@ -434,13 +447,14 @@ always_comb begin
 		default: mem_out = 8'd0;
 	endcase
 
-    casex({tapeloader, mem_size, chr128_mem })
-        'b1_XX_X: ram_a = tape_load_addr;
-        'b0_00_0: ram_a = { 6'b010000,             addr[9:0] }; //1k
-        'b0_01_0: ram_a = { 2'b01,                addr[13:0] }; //16k
-        'b0_10_0: ram_a = { 1'b0, addr[15] & nM1, addr[13:0] } + 16'h4000; //32k
-        'b0_11_0: ram_a = { addr[15] & nM1,       addr[14:0] }; //64k
-        'b0_XX_1: ram_a = nRFSH ? addr[15:0] : { addr[15:10], addr[8] & ram_data_latch[7], ram_data_latch[5:0], row_counter }; //chr
+    casex({tapeloader, mem_size, chr128_mem, chrqs_mem})
+        'b1_XX_XX: ram_a = tape_load_addr;
+        'b0_00_00: ram_a = { 6'b010000,             addr[9:0] }; //1k
+        'b0_01_00: ram_a = { 2'b01,                addr[13:0] }; //16k
+        'b0_10_00: ram_a = { 1'b0, addr[15] & nM1, addr[13:0] } + 16'h4000; //32k
+        'b0_11_00: ram_a = { addr[15] & nM1,       addr[14:0] }; //64k
+        'b0_XX_1X: ram_a = nRFSH ? addr[15:0] : { addr[15:11], addr[9], addr[8] & ram_data_latch[7], ram_data_latch[5:0], row_counter }; //chr128
+        'b0_XX_01: ram_a = nRFSH ? addr[15:0] : { 4'h8, 2'b01, ram_data_latch[7], ram_data_latch[5:0], row_counter }; //chrqs
     endcase
 end
 
